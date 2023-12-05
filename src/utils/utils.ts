bf0ca4c8e5fb8533b6472fa7def2b73e0d6b9083
Tim Hollingshead <thollingshead@esri.com>
2023-12-05 12:23:22 -0700

    Revert "Prevent full JS API library from being bundled"
    This reverts commit b8e137498876f0251628509436c8f42b9d1e786d.

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -9,21 +9,14 @@ function requireModule<T>(modulePath: string): Promise<T> {
 
 export async function importEsri(modulePath: string): Promise<any> {
   const relativeModulePath = modulePath.replace(/^(@arcgis\/core\/|esri\/)/, '');
-
-  /**
-   * Removes the partial path information from the import statement, thus
-   * preventing webpack from bundling the entire Esri library.
-   * @see https://webpack.js.org/api/module-methods/#dynamic-expressions-in-import
-   */
-  const fullModulePath = `${amd ? 'esri' : '@arcgis/core'}/${relativeModulePath}`;
   if (amd) {
-    return requireModule(fullModulePath);
+    return requireModule(`esri/${relativeModulePath}`);
   }
 
   /**
    * @see https://webpack.js.org/api/module-methods/#webpackignore
    */
-  const module = await import(/* webpackIgnore: true */ fullModulePath);
+  const module = await import(/* webpackIgnore: true */ `@arcgis/core/${relativeModulePath}`);
   if (module.default) {
     return module.default;
   }
